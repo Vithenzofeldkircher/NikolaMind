f34be2e858135bc03d7edb7714c2be6bff190aa4
Melissa Zanotto <melissa.angerer@alunos.sc.senac.br>
2026-02-06 19:01:27 -0300

    Code Menu

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -3,58 +3,33 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    [Header("Nomes das Cenas")]
-    [Tooltip("Nome da cena principal de jogabilidade.")]
-    public string nomeCenaPrincipal;
-
-    [Tooltip("Nome da cena de configurações e opções.")]
-    public string nomeCenaOpcoes;
-
-    [Tooltip("Nome da cena que exibe os créditos do jogo.")]
-    public string nomeCenaCreditos;
-
-    public void CarregarCenaPrincipal()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Painel de Configurações")]
+    public GameObject painelConfiguraçoes;
+    
+    //Ir para a ouitra cena
+    public void AbrirCena(string nomeDaaCena)
     {
-        if (!string.IsNullOrEmpty(nomeCenaPrincipal))
-        {
-            SceneManager.LoadScene(nomeCenaPrincipal);
-        }
-        else
-        {
-            Debug.LogError("O nome da cena Principal não foi definido no Inspector!");
-        }
+        SceneManager.LoadScene(nomeDaaCena);
     }
 
-    public void CarregarCenaOpcoes()
+    //Abrir painel
+    public void AbrirConfiguracoes()
     {
-        if (!string.IsNullOrEmpty(nomeCenaOpcoes))
-        {
-            SceneManager.LoadScene(nomeCenaOpcoes);
-        }
-        else
-        {
-            Debug.LogError("O nome da cena de Opções não foi definido no Inspector!");
-        }
+        painelConfiguraçoes.SetActive(true);
+    }
+    public void FecharConfiguracoes()
+    {
+        painelConfiguraçoes.SetActive(false);
     }
 
-    public void CarregarCenaCreditos()
-    {
-        if (!string.IsNullOrEmpty(nomeCenaCreditos))
-        {
-            SceneManager.LoadScene(nomeCenaCreditos);
-        }
-        else
-        {
-            Debug.LogError("O nome da cena de Créditos não foi definido no Inspector!");
-        }
-    }
-
+    //Sair do jogo
     public void SairDoJogo()
     {
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#else
         Application.Quit();
-#endif
+
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
     }
 }
