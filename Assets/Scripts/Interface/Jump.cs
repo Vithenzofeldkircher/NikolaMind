91545d4d3b693b409e01c9dfa4f3620e14b5df3a
Vithenzofeldkircher <vithenzo.feldkircher@alunos.sc.senac.br>
2026-02-11 10:46:28 -0300

    Update
    Bugs com o sistema de pulo "Inicial"

--- Assets/Scripts/Interface/Jump.cs
+++ Assets/Scripts/Interface/Jump.cs
@@ -2,5 +2,7 @@ using UnityEngine;
 
 public interface Jump
 {
+    public bool Can_Jump();
+    public void Jumps();
     public void OnCollisionEnter2D(Collision2D collision);
 }
--- Assets/Scripts/Player_Move.cs
+++ Assets/Scripts/Player_Move.cs
@@ -8,27 +8,57 @@ public class Player_Move : MonoBehaviour, Jump
     public float _speed = 2.0f;
     private Vector2 _movement;
     private Rigidbody2D _rb;
-    private int _Jump_Force = 17f;
+    private int Count_Jump;
+
+    [SerializeField] private float _Jump_Force = 17f;
+    [SerializeField] private int Max_Jump = 1;
+
+
+    private bool Is_Ground;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        _movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Jump"));
+        _movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxisRaw("Jump"));
+
+       
     }
 
     private void FixedUpdate()
     {
         _rb.linearVelocity = _movement * _speed;
+
+        if (Input.GetButtonDown("Jump") && Is_Ground)
+        {
+            _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, _Jump_Force);
+        }
+        
+    }
+
+    public bool Can_Jump()
+    {
+        return Max_Jump < Count_Jump;
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision)
+        //verifica se há um chão
+        if(collision != null && !Is_Ground)
+        {
+            Is_Ground = true;
+            Debug.Log("Há chão");
+        }
+    }
+
+    public void Jumps()
+    {
+        throw new System.NotImplementedException();
     }
 }
