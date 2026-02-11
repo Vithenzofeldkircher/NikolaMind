03d4943d78e41929ef0a1ae2e5d82b7b5bab9fc5
Vithenzofeldkircher <vithenzo.feldkircher@alunos.sc.senac.br>
2026-02-11 09:39:14 -0300

    update, pulos

---
+++ Assets/Scripts/Interface/Jump.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public interface Jump
+{
+    public void OnCollisionEnter2D(Collision2D collision);
+}
--- Assets/Scripts/Player_Move.cs
+++ Assets/Scripts/Player_Move.cs
@@ -2,12 +2,13 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class Player_Move : MonoBehaviour
+public class Player_Move : MonoBehaviour, Jump
 {
-
-    private float _speed = 2.0f;
+    [Header("Movement")]
+    public float _speed = 2.0f;
     private Vector2 _movement;
     private Rigidbody2D _rb;
+    private int _Jump_Force = 17f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,11 +19,16 @@ public class Player_Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        _movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Jump"));
     }
 
     private void FixedUpdate()
     {
         _rb.linearVelocity = _movement * _speed;
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision)
     }
 }
