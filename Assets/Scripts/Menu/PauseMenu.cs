438401cbe061fa3d4f76f7cadaedfac77cd74523
Melissa Zanotto <melissa.angerer@alunos.sc.senac.br>
2026-08-11 20:59:39 -0300

    Implement pause menu

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    private static PauseMenu instance;
+
+    public GameObject pausePanel;
+
+    public static bool isGamePaused = false;
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isGamePaused)
+            {
+                Voltar();
+            }
+            else
+            {
+                AbrirPause();
+            }
+        }
+    }
+
+    public void AbrirPause()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+
+    public void Voltar()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+    public void IrParaMenu()
+    {
+        Time.timeScale = 1f;
+        isGamePaused = false;
+        SceneManager.LoadScene("telaInicial");
+    }
+
+    public void SairDoJogo()
+    {
+        Application.Quit();
+    }
+}
