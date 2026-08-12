c79748f18f079484de7e4f0bfeb9395c086774b9
Melissa Zanotto <melissa.angerer@alunos.sc.senac.br>
2026-08-11 22:23:56 -0300

    Add save system foundation

---
+++ Assets/Scripts/Menu/SaveData.cs
@@ -0,0 +1,17 @@
+[System.Serializable]
+public class SaveData
+{
+    public string sceneName;
+
+    public float playerPosX;
+    public float playerPosY;
+    public float playerPosZ;
+
+    public float playerRotX;
+    public float playerRotY;
+    public float playerRotZ;
+
+    public float playerHealth;
+
+    public float playTime;
+}
